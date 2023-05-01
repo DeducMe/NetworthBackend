43c646483da9bccf70f8cbe45d04326d38ac656f
DeducMe <deducme@gmail.com>
2023-05-01 20:44:40 +0400

    put assets

--- source/api/assets/assets/assetsController.ts
+++ source/api/assets/assets/assetsController.ts
@@ -4,6 +4,34 @@ import Assets from './assetsModal';
 import { createAssetsAdditionalTypesItem } from '../assetsAdditional/assetsAdditionalTypesItem/assetsAdditionalTypesItemController';
 import profileModal from '../../users/profile/profileModal';
 
+const put = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        let { _id, name, categories, price, image, additional, currency, amount } = req.body;
+
+        // let additionalLooped = [];
+
+        // TODO добавить additional
+        // for (const item of additional) {
+        //     additionalLooped.push(await createAssetsAdditionalTypesItem(item));
+        // }
+
+        const decoded = await decodeToken(req?.headers?.authorization || '');
+        if (!decoded) return errorHandler(res, { message: 'decode of auth header went wrong' }, 500);
+
+        const profile = await profileModal.findOne({ userId: decoded.id });
+        if (!profile) return errorHandler(res, { message: 'decode of auth header went wrong' }, 500);
+
+        const assetToUpdate = await Assets.findOne({ userProfileId: profile.id, _id: _id });
+        if (!assetToUpdate) return errorHandler(res, { message: 'Asset was not found' }, 422);
+
+        const data = await assetToUpdate.updateOne({ name, amount, categories, price, image, currency }).exec();
+
+        sendBackHandler(res, 'assets', data);
+    } catch (e) {
+        errorHandler(res, e);
+    }
+};
+
 const create = async (req: Request, res: Response, next: NextFunction) => {
     try {
         let { name, categories, price, image, additional, currency, amount } = req.body;
@@ -38,13 +66,13 @@ const getAll = async (req: Request, res: Response, next: NextFunction) => {
 
     const additionalFilters: any = {};
 
-    if (filters.priceMin || filters.priceMax) {
+    if (filters?.priceMin || filters?.priceMax) {
         additionalFilters.price = {};
         if (filters.priceMin) additionalFilters.price.$lt = filters?.priceMin;
         if (filters.priceMax) additionalFilters.price.$gte = filters?.priceMax;
     }
 
-    if (filters.categories) {
+    if (filters?.categories) {
         additionalFilters.categories = { $in: filters.categories };
     }
 
@@ -65,4 +93,4 @@ const getAll = async (req: Request, res: Response, next: NextFunction) => {
     sendBackHandler(res, 'assets', data);
 };
 
-export default { getAll, create };
+export default { getAll, create, put };
